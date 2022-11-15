7077981c5e07e0968627fdc31a4e90e3db2c1504
pauldavies83 <pauldavies83@googlemail.com>
2022-11-15 15:39:58 +0000

    🍎⭐ ️display breeds in a sheet. select a specific breed and only show images for that breed

---
+++ iosApp/iosApp/Screen/KatzList/BreedList.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+import shared
+
+struct BreedList: View {
+    let breeds: [BreedDrawerItem]
+    let onItemSelected: () -> Void
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(alignment: .leading, spacing: 4, pinnedViews: [.sectionHeaders]) {
+                Section(header:
+                    HStack {
+                        Text("Breeds")
+                            .font(Font.title2)
+                            .foregroundColor(Color.primary)
+                        Spacer()
+                        Button(action: { onItemSelected() }) {
+                            Image(systemName: "xmark")
+                                .foregroundColor(Color.primary)
+                                .padding(.horizontal, 16)
+                        }
+                    }
+                    .padding(16)
+                    .opacity(1.0)
+                    .background(Color.secondary)
+                ) {
+                    ForEach(breeds, id: \.id) { breed in
+                        Button(action: {
+                            breed.onClick()
+                            onItemSelected()
+                        }) {
+                            HStack {
+                                Text(breed.name)
+                                    .foregroundColor(Color.primary)
+                                    .padding(16)
+                                if (breed.selected) {
+                                    Spacer()
+                                    Image(systemName: "star.fill")
+                                        .foregroundColor(Color.primary)
+                                        .padding(16)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- iosApp/iosApp/Screen/KatzList/KatzListScreen.swift
+++ iosApp/iosApp/Screen/KatzList/KatzListScreen.swift
@@ -3,6 +3,7 @@ import shared
 
 struct KatzListScreen: View {
     @ObservedObject private var viewModel = KatzListViewModel()
+    @State private var showSheet = false
 
 	var body: some View {
         ZStack {
@@ -12,6 +13,28 @@ struct KatzListScreen: View {
         }
         .onAppear { viewModel.onAppear() }
         .onDisappear { viewModel.onDisappear() }
+        .sheet(isPresented: $showSheet) {
+            if let breeds = viewModel.state.breeds {
+                BreedList(
+                    breeds: breeds,
+                    onItemSelected: { showSheet = false }
+                )
+            }
+        }
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                HStack {
+                    Button(action: { showSheet = !showSheet }) {
+                        Image(systemName: "arrow.up.circle")
+                            .foregroundColor(Color.primary)
+                    }
+                    Spacer()
+                    Text(viewModel.state.title ?? "Katz")
+                    Spacer()
+                    HStack {}
+                }
+            }
+        }
     }
 }
 
